685cf5b357bcce980c80b438a187d813d0cd80f1
Kurnevsky Evgeny <kurnevsky@gmail.com>
2016-12-14 13:26:53 +0300

    Remove xrandr call.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -207,7 +207,6 @@ myStartupHook = do
   addScreenCorner SCLowerLeft $ gridselectWorkspace myGSConfig W.greedyView
   spawn "setxkbmap -model pc101 -layout us,ru -option grp:caps_toggle -option grp:switch -option grp_led:caps -option lv3:ralt_switch"
   spawn "sleep 1; xmodmap ~/.Xmodmap"
-  spawn "xrandr --setprovideroffloadsink 0x50 0x79"
   spawn "compton -b -f -I 0.10 -O 0.10 --backend glx --vsync opengl --dbus"
   spawn "feh --bg-fill ~/Images/pic-3909-1920x1200.jpg"
   spawn "taffybar"
