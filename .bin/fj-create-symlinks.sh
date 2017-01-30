66cf5f7b342943aa499584c7fa9ed3fa26cfdf6f
Kurnevsky Evgeny <kurnevsky@gmail.com>
2017-01-30 19:24:22 +0300

    Add maxdepth parameter.

--- .bin/fj-create-symlinks.sh
+++ .bin/fj-create-symlinks.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-APPS=$(find /etc/firejail ~/.config/firejail -type f -printf "%f\n" | egrep '\.profile$' | rev | cut -c 9- | rev | sort -u)
+APPS=$(find /etc/firejail ~/.config/firejail -maxdepth 1 -type f -printf "%f\n" | egrep '\.profile$' | rev | cut -c 9- | rev | sort -u)
 for APP in $APPS; do
     if [ -e "/bin/$APP" ]; then
         ln -s /bin/firejail /usr/local/bin/"$APP"
