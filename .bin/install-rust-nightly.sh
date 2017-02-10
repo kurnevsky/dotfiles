36b6454d5e94c90613b433cdd3668975bfc853e1
Kurnevsky Evgeny <kurnevsky@gmail.com>
2017-02-10 09:04:44 +0300

    Update rust installation script.

--- .bin/install-rust-nightly.sh
+++ .bin/install-rust-nightly.sh
@@ -1,31 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 wget https://static.rust-lang.org/dist/rustc-nightly-src.tar.gz
 wget https://static.rust-lang.org/dist/rustc-nightly-src.tar.gz.asc
-gpg --verify rustc-nightly-src.tar.gz{.asc*,}
-
-if [ $status -ne 0 ]; then
-    echo "Can't verify rustc-nightly-src" >&2
-    return
-fi
+gpg --verify rustc-nightly-src.tar.gz{.asc,} || { echo "Can't verify rustc-nightly-src" >&2; exit 1; }
 
 wget https://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz
 wget https://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz.asc
-gpg --verify rust-nightly-x86_64-unknown-linux-gnu.tar.gz{.asc*,}
-
-if [ $status -ne 0 ]; then
-    echo "Can't verify rust-nightly-x86_64-unknown-linux-gnu" >&2
-    return
-fi
+gpg --verify rust-nightly-x86_64-unknown-linux-gnu.tar.gz{.asc,} || { echo "Can't verify rust-nightly-x86_64-unknown-linux-gnu" >&2; exit 1; }
 
 rm -rf rust
 rm -rf rust-nightly-src
 rm -rf rust-nightly-x86_64-unknown-linux-gnu
 
 tar -xvzf rustc-nightly-src.tar.gz
-mv rustc-nightly-src rust-nightly-src
-rm rustc-nightly-src.tar.gz
-
 tar -xvzf rust-nightly-x86_64-unknown-linux-gnu.tar.gz
-rm rust-nightly-x86_64-unknown-linux-gnu.tar.gz
-./rust-nightly-x86_64-unknown-linux-gnu/install.sh --prefix=~/rust
+
+rm rustc-nightly-src.tar.gz{.asc,}
+rm rust-nightly-x86_64-unknown-linux-gnu.tar.gz{.asc,}
+
+mv rustc-nightly-src rust-nightly-src
+
+./rust-nightly-x86_64-unknown-linux-gnu/install.sh --prefix=./rust
