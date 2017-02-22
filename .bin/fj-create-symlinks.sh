3e40e0fb378cf5c6b7073cc0ae674f043ddb81ce
Kurnevsky Evgeny <kurnevsky@gmail.com>
2017-02-22 21:20:15 +0300

    Update fj-create-symlinks script.

--- .bin/fj-create-symlinks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-APPS=$(find .config/firejail -maxdepth 1 -type f -printf "%f\n" | egrep '\.profile$' | rev | cut -c 9- | rev | sort -u)
-for APP in $APPS; do
-    if [ -e "/bin/$APP" ]; then
-        ln -s /bin/firejail /usr/local/bin/"$APP"
-    fi
-done
