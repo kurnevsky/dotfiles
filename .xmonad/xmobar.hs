4b9ef1ff2931f0d735ef199b909d570733da6e41
Evgeny Kurnevsky <kurnevsky@gmail.com>
2017-10-26 09:32:42 +0300

    Use st instead of alacritty.

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -54,6 +54,6 @@ Config { font = "xft:DejaVu Sans:pixelsize=14:antialias=true:autohint=false"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       -- , template = "%trayer% : %UnsafeStdinReader%}{%mpris2% : %dynnetwork% : %diskio% : %cpu% : %memory% : %swap% : %battery% : %UMMS% : <action=`alacritty -e zsh -c 'cal -m -y; sleep infinity'`>%date%</action>"
-       , template = "%trayer% : %UnsafeStdinReader%}{%dynnetwork% : %diskio% : %cpu% : %memory% : %swap% : %battery% : %UMMS% : <action=`alacritty -e zsh -c 'cal -m -y; sleep infinity'`>%date%</action>"
+       -- , template = "%trayer% : %UnsafeStdinReader%}{%mpris2% : %dynnetwork% : %diskio% : %cpu% : %memory% : %swap% : %battery% : %UMMS% : <action=`st -e zsh -c 'cal -m -y; sleep infinity'`>%date%</action>"
+       , template = "%trayer% : %UnsafeStdinReader%}{%dynnetwork% : %diskio% : %cpu% : %memory% : %swap% : %battery% : %UMMS% : <action=`st -e zsh -c 'cal -m -y; sleep infinity'`>%date%</action>"
        }
