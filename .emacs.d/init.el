3e0eacf0809e832175ab0f9d71e0cab23046511c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2017-11-28 17:25:06 +0300

    Remove sbt mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -433,16 +433,6 @@
                                 (local-set-key (kbd "-") 'left-arrow)
                                 (local-set-key (kbd ">") 'right-arrow))))
 
-(use-package sbt-mode
-  :commands (sbt-start sbt-command)
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-    'minibuffer-complete-word
-    'self-insert-command
-    minibuffer-local-completion-map))
-
 (use-package haskell-mode
   :mode ("\\.hs\\'" . haskell-mode)
   :interpreter ("ghci" . haskell-mode))
