3e288f9aabb40383d489b190600e06d16e4c65b0
Evgeny Kurnevsky <kurnevsky@gmail.com>
2018-11-17 11:47:45 +0300

    Update firefox config.

--- .mozilla/firefox/default/user.js
+++ .mozilla/firefox/default/user.js
@@ -1,20 +1,14 @@
 // Show a blank page when Firefox starts
-user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.startup.page", 0);
-user_pref("browser.rights.3.shown", true);
+user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.startup.homepage_override.mstone", "ignore");
 user_pref("startup.homepage_welcome_url", "");
 user_pref("startup.homepage_welcome_url.additional", "");
 user_pref("startup.homepage_override_url", "");
-user_pref("browser.laterrun.enabled", false);
 
 // Show a blank page when opening new tab
 user_pref("browser.newtabpage.enabled", false);
-user_pref("browser.newtabpage.enhanced", false);
 user_pref("browser.newtab.preload", false);
-user_pref("browser.newtabpage.introShown", true);
-user_pref("browser.newtabpage.directory.source", "");
-user_pref("browser.newtabpage.activity-stream.enabled", false);
 
 // Disable Pocket
 user_pref("extensions.pocket.enabled", false);
@@ -24,6 +18,47 @@ user_pref("browser.download.useDownloadDir", false);
 
 // Request that sites not track me
 user_pref("privacy.donottrackheader.enabled", true);
+
+// Enable Tracking Protection.
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.pbmode.enabled", true);
+user_pref("privacy.trackingprotection.introCount", 20);
+
+// Enable Content Blocking.
+user_pref("browser.contentblocking.enabled", true);
+user_pref("browser.contentblocking.introCount", 20);
+
+// Enable First Party Isolation.
+user_pref("privacy.firstparty.isolate", true);
+user_pref("privacy.firstparty.isolate.restrict_opener_access", true);
+
+// Disable Health Report.
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+
+// Disable Crash Reporter.
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+
+// Disable Browser Error Reporter.
+user_pref("browser.chrome.errorReporter.enabled", false);
+user_pref("browser.chrome.errorReporter.submitUrl", "");
+
+// Disable Telemetry.
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.cachedClientID", "");
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
 
 // Enable OpenGL Off-Main-Thread Compositing (OMTC)
 user_pref("layers.acceleration.force-enabled", true);
