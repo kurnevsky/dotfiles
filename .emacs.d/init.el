c5021861540a79e577a51bde35419e7d4ff3e221
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-03-27 12:44:41 +0300

    Simplify the toolbar disabling.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,14 +19,8 @@
 ;; Start in maximized window mode.
 (toggle-frame-maximized)
 ;; Disable tool bar.
-(if (daemonp)
-  (add-hook 'after-make-frame-functions
-    (lambda (frame)
-      (with-selected-frame frame
-        (when (display-graphic-p)
-          (tool-bar-mode -1)))))
-  (when (display-graphic-p)
-    (tool-bar-mode -1)))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 ;; Set font.
 (set-face-attribute 'default nil :font "DejaVu Sans Mono:pixelsize=15")
 (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono:pixelsize=15"))
