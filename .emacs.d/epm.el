ddf27c54e7766465e9cc49525740965cf6336712
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-08-09 22:48:00 +0300

    Fix for bug 34341.

--- .emacs.d/epm.el
+++ .emacs.d/epm.el
@@ -1,7 +1,15 @@
 (require 'package)
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-  '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; Configure the list of remote archives.
+(dolist (archive '(("melpa" . "https://melpa.org/packages/")
+                    ("melpa-stable" . "https://stable.melpa.org/packages/")))
+  (add-to-list 'package-archives archive))
+(setq package-archive-priorities
+  '(("melpa" . 2)
+     ("melpa-stable" . 1)
+     ("gnu" . 0)))
+;; Fix for bug 34341.
+(when (version<= "26" emacs-version)
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 ;; Activate all the packages (in particular autoloads).
-(package-initialize)
+(package-initialize t)
+(package-activate 'epm)
