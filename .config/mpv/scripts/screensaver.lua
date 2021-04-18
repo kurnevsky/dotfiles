8da5f8b93a514ba41b6f2281d26c8941aa57fe0a
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-04-18 19:32:35 +0300

    Rewrite mpv screensaver script.

--- .config/mpv/scripts/screensaver.lua
+++ .config/mpv/scripts/screensaver.lua
@@ -1,13 +1,36 @@
 local utils = require 'mp.utils'
+local window_id;
 
-local cmd = "sleep 1; wmctrl -lx | awk '{ORS=\" \"; print $1; system(\"xcb-client-id \" $1)}' | grep " .. utils.getpid() .. " | cut -d\" \" -f1 | head -n1 | xargs -r xdg-screensaver "
+function suspend()
+   os.execute("xdg-screensaver suspend " .. window_id)
+end
+
+function resume()
+   os.execute("xdg-screensaver resume " .. window_id)
+end
 
 function on_pause_change(name, value)
    if value then
-      os.execute(cmd .. "resume")
+      resume()
    else
-      os.execute(cmd .. "suspend")
+      suspend()
    end
 end
 
-mp.observe_property("pause", "bool", on_pause_change)
+function find_window_id()
+   local handle = io.popen("wmctrl -lx | awk '{ORS=\" \"; print $1; system(\"xcb-client-id \" $1)}' | grep " .. utils.getpid() .. " | cut -d\" \" -f1 | head -n1", 'r')
+   local result = handle:read("*a")
+   handle:close()
+   if result ~= "" then
+      window_id = result:gsub("%s+", " ")
+      print("MPV window id is " .. window_id)
+      if not mp.get_property_native("pause") then
+         suspend()
+      end
+      mp.observe_property("pause", "bool", on_pause_change)
+   else
+      mp.add_timeout(1, find_window_id)
+   end
+end
+
+mp.add_timeout(1, find_window_id)
